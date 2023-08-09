95ca113716c56e32a19853770ee8a4d030b72765
Paul Jimenez <pj@place.org>
2023-08-08 23:03:49 -0400

    add tag and attr searching to find() util

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -5,11 +5,28 @@ def test_find():
     p = parse(
         """
     <doc>a
-      <baz>f
-        <b>o</b>ob
+      <baz d="1">f
+        <b d="2">o</b>ob
         <b>a</b>r
       </baz>a
     </doc>"""
     )
 
-    assert len(list(find(p, "b"))) == 2
+    found = list(find(p))
+    assert len(found) == 3
+    assert [t._name for t in found] == ['baz', 'b', 'b']
+
+    found = list(find(p, tag="b"))
+    assert len(found) == 2
+    assert [t._name for t in found] == ['b', 'b']
+
+    found = list(find(p, attr="d"))
+    assert len(found) == 2
+    assert [t._name for t in found] == ['baz', 'b']
+
+    found = list(find(p, attr="d=1"))
+    assert len(found) == 1
+    assert found[0]._name == 'baz'
+    assert len(list(e for e in found[0] if not isinstance(e, str))) == 2
+
+
--- xmltread/utils.py
+++ xmltread/utils.py
@@ -1,12 +1,33 @@
 from .xmltramp import Element
 
+import logging
 
-def find(e: Element, tag):
-    """generator: find all the tags with the name 'tag'"""
+logger = logging.getLogger(__name__)
+
+
+def find(e: Element, tag=None, attr=None):
+    """generator: find some tags
+        * if tag is specified, returned tags must have that name
+        * if attr is specified,
+          * if attr is of the form 'name=value' then the attr of the specified name must have the specified value
+          * otherwise the attr must exist
+        * if no params are specified, match all the tags
+    """
     for t in e:
         if not isinstance(t, Element):
+            logger.info('skipping %r', t)
             continue
-        if t._name == tag:
-            yield tag
-        else:
-            yield from find(t, tag)
+        if tag is not None and t._name == tag:
+            logger.info('yielding matching tag %r', t)
+            yield t
+        elif attr is not None:
+            k, *v = attr.split('=')
+            if k in t._attrs and (v == [] or t._attrs[k] == v[0]):
+                logger.info('yielding tag matching attrs %r=%r %r', k, v, t)
+                yield t
+            else:
+                logger.info('%r=%r not found in %r', k, v, t._attrs)
+        elif tag is None and attr is None:
+            yield t
+        logger.info('descending into tag %r', t)
+        yield from find(t, tag, attr)
